d8d58d40ba74877b282a0bdd351798309f35c732
Steven G. Harris <steven.g.harris@gmail.com>
2023-11-11 09:27:22 -0800

    Initial Commit

---
+++ DateSlider/DateSlider.h
@@ -0,0 +1,18 @@
+//
+//  DateSlider.h
+//  DateSlider
+//
+//  Created by Steven Harris on 11/11/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DateSlider.
+FOUNDATION_EXPORT double DateSliderVersionNumber;
+
+//! Project version string for DateSlider.
+FOUNDATION_EXPORT const unsigned char DateSliderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DateSlider/PublicHeader.h>
+
+
