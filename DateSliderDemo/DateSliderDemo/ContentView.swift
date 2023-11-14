596b884bba09e3bb336f231c6922bbbea6b54e5f
Steven G. Harris <steven.g.harris@gmail.com>
2023-11-14 09:40:31 -0800

    Add DateSliderDemo

--- DateSliderDemo/DateSliderDemo/ContentView.swift
+++ DateSliderDemo/DateSliderDemo/ContentView.swift
@@ -6,16 +6,42 @@
 //
 
 import SwiftUI
+import DateSlider
 
 struct ContentView: View {
+    
+    struct DatedString: DatedObject {
+        var date: Date
+        var string: String
+    }
+    
+    let dates = [
+        Date(),
+        Date().addingTimeInterval(100000),
+        Date().addingTimeInterval(200000),
+    ]
+    
+    let datedStrings: [DatedString] = [
+        DatedString(date: Date(), string: "foo"),
+        DatedString(date: Date().addingTimeInterval(100000), string: "bar"),
+        DatedString(date: Date().addingTimeInterval(200000), string: "baz"),
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { geometry in
+            VStack {
+                DateSliderView(datedObjects: dates, format: .shortDateLocal, onDateSelect: dateSelected(_:))
+                    .frame(width: geometry.size.width, height: 30)
+                DateSliderView(datedObjects: datedStrings, format: .shortDateTimeLocal, onDateSelect: dateSelected(_:))
+                    .frame(width: geometry.size.width, height: 30)
+                Spacer()
+            }
         }
         .padding()
+    }
+    
+    func dateSelected(_ date: Date) {
+        print("Selected \(date)")
     }
 }
 
